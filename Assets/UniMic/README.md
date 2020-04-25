bd44c30a48827c335c25da2027b7270356669cfe
Vatsal Ambastha <adrenak_@outlook.com>
2020-04-26 00:10:00 +0530

    feat: Mic class now only handles Mic audio streaming and does not have an internal AudioSource
    MicAudioSource.cs added to play Mic stream in-game
    
    BREAKING CHANGE: Internal AudioSource and related methods deleted

--- Assets/UniMic/README.md
+++ Assets/UniMic/README.md
@@ -1,9 +1,9 @@
+![Cover](https://github.com/adrenak/UniMic/blob/master/cover.jpg)
 ## UniMic
-A convenience wrapper for Unity's Microphone class.
+A wrapper for Unity's Microphone class.
 
 ## API
 `Mic` class in the `Adrenak.UniMic` namespace is a singleton and is accessed using `Mic.Instance`
-
 
 ### Properties
 - `IsRecording` 
@@ -18,10 +18,10 @@ The last populated sample of the audio data
 - `SampleDurationMS`
 The duration of the sample segment in milliseconds that the instance maintains and fires in events. 
 
-- `SampleLength`
+- `SampleLen`
 The number of samples in the sample segment
 
-- `AudioClip`
+- `Clip`
 The inner `AudioClip` of the instance
 
 - `Devices`
@@ -42,11 +42,9 @@ Event fired when the instance starts to record the audio
 Event fired when the instance stops recording the audio
 
 - `OnSampleReady`
-Event fired when a sample of `SampleLength` has been populated by the instance
+Event fired when a sample of `SampleLen` has been populated by the instance
 
 ### Methods
-- `UpdateDevices` updates the list of available recording devices. 
-
 - `ChangeDevice` changes the recording device. The method internally restarts the recording process
     - `Arguments`
         - `int index` the index of the device in the `Devices` list
@@ -65,8 +63,25 @@ Event fired when a sample of `SampleLength` has been populated by the instance
     - `Returns`
         - `void`
 
+- `GetSpectrumData` provides a block of the microphone input spectrum data.
+    - `Arguments`
+        - `FFTWindow fftWindow` the Fast Fourier Transform window to be used. [Info](https://docs.unity3d.com/ScriptReference/FFTWindow.html)
+        - `int sampleCount` the sample count for the internal `AudioSource.GetSpectrumData` call
+    - `Returns`
+        - `float[]` the spectrum data
+
+- `GetOutputData` provides a block of the microphone input output data.
+    - `Arguments`
+        - `int sampleCount` the sample count for the internal `AudioSource.GetOutputData` call
+    - `Returns`
+        - `float[]` the output data
+
+## Tips
+Just open the Unity project in Unity5+ and try the demo scene.  
+
+## Soon  
+Ready to use spectrum methods such as for standard octaves
 
 ## Contact
 [@github](https://www.github.com/adrenak)  
-[@www](http://www.vatsalambastha.com)  
-[@npm](http://www.npmjs.com/~adrenak)
+[@www](http://www.vatsalambastha.com)
---
+++ Assets/UniMic/Runtime/Mic.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Adrenak.UniMic {
+    [RequireComponent(typeof(AudioSource))]
+    public class Mic : MonoBehaviour {
+        // ================================================
+        #region MEMBERS
+        // ================================================
+        /// <summary>
+        /// Whether the microphone is running
+        /// </summary>
+        public bool IsRecording { get; private set; }
+
+        /// <summary>
+        /// The frequency at which the mic is operating
+        /// </summary>
+        public int Frequency { get; private set; }
+
+        /// <summary>
+        /// Last populated audio sample
+        /// </summary>
+        public float[] Sample { get; private set; }
+
+        /// <summary>
+        /// Sample duration/length in milliseconds
+        /// </summary>
+        public int SampleDurationMS { get; private set; }
+
+        /// <summary>
+        /// The length of the sample float array
+        /// </summary>
+        public int SampleLength {
+            get { return Frequency * SampleDurationMS / 1000; }
+        }
+
+        /// <summary>
+        /// The AudioClip currently being streamed in the Mic
+        /// </summary>
+        public AudioClip AudioClip { get; private set; }
+
+        /// <summary>
+        /// List of all the available Mic devices
+        /// </summary>
+        public List<string> Devices { get; private set; }
+
+        /// <summary>
+        /// Index of the current Mic device in m_Devices
+        /// </summary>
+        public int CurrentDeviceIndex { get; private set; }
+
+        /// <summary>
+        /// Gets the name of the Mic device currently in use
+        /// </summary>
+        public string CurrentDeviceName {
+            get { return Devices[CurrentDeviceIndex]; }
+        }
+
+        int m_SampleCount = 0;
+        #endregion
+
+        // ================================================
+        #region EVENTS
+        // ================================================
+        /// <summary>
+        /// Invoked when the instance starts Recording.
+        /// </summary>
+        public event Action OnStartRecording;
+
+        /// <summary>
+        /// Invoked everytime an audio frame is collected. Includes the frame.
+        /// </summary>
+        public event Action<int, float[]> OnSampleReady;
+
+        /// <summary>
+        /// Invoked when the instance stop Recording.
+        /// </summary>
+        public event Action OnStopRecording;
+        #endregion
+
+        // ================================================
+        #region METHODS
+        // ================================================
+
+        static Mic m_Instance;
+        public static Mic Instance {
+            get {
+                if (m_Instance == null)
+                    m_Instance = GameObject.FindObjectOfType<Mic>();
+                if (m_Instance == null) {
+                    m_Instance = new GameObject("UniMic.Mic").AddComponent<Mic>();
+                    DontDestroyOnLoad(m_Instance.gameObject);
+                }
+                return m_Instance;
+            }
+        }
+
+        public static Mic Instantiate() {
+            return Instance;
+        }
+
+        void Awake() {
+            UpdateDevices();
+            CurrentDeviceIndex = 0;
+        }
+
+        public void UpdateDevices() {
+            Devices = new List<string>();
+            foreach (var device in Microphone.devices)
+                Devices.Add(device);
+        }
+
+        /// <summary>
+        /// Changes to a Mic device for Recording
+        /// </summary>
+        /// <param name="index">The index of the Mic device. Refer to <see cref="Devices"/></param>
+        public void ChangeDevice(int index) {
+            Microphone.End(CurrentDeviceName);
+            CurrentDeviceIndex = index;
+            StartRecording(Frequency, SampleDurationMS);
+        }
+
+        /// <summary>
+        /// Starts to stream the input of the current Mic device
+        /// </summary>
+        public void StartRecording(int frequency = 16000, int sampleLen = 10) {
+            StopRecording();
+            IsRecording = true;
+
+            Frequency = frequency;
+            SampleDurationMS = sampleLen;
+
+            AudioClip = Microphone.Start(CurrentDeviceName, true, 1, Frequency);
+            Sample = new float[Frequency / 1000 * SampleDurationMS * AudioClip.channels];
+
+            //m_AudioSource.clip = Clip;
+
+            StartCoroutine(ReadRawAudio());
+
+            if (OnStartRecording != null)
+                OnStartRecording.Invoke();
+        }
+
+        /// <summary>
+        /// Ends the Mic stream.
+        /// </summary>
+        public void StopRecording() {
+            if (!Microphone.IsRecording(CurrentDeviceName)) return;
+
+            IsRecording = false;
+
+            Microphone.End(CurrentDeviceName);
+            Destroy(AudioClip);
+            AudioClip = null;
+
+            StopCoroutine(ReadRawAudio());
+
+            if (OnStopRecording != null)
+                OnStopRecording.Invoke();
+        }
+
+        IEnumerator ReadRawAudio() {
+            int loops = 0;
+            int readAbsPos = 0;
+            int prevPos = 0;
+            float[] temp = new float[Sample.Length];
+
+            while (AudioClip != null && Microphone.IsRecording(CurrentDeviceName)) {
+                bool isNewDataAvailable = true;
+
+                while (isNewDataAvailable) {
+                    int currPos = Microphone.GetPosition(CurrentDeviceName);
+                    if (currPos < prevPos)
+                        loops++;
+                    prevPos = currPos;
+
+                    var currAbsPos = loops * AudioClip.samples + currPos;
+                    var nextReadAbsPos = readAbsPos + temp.Length;
+
+                    if (nextReadAbsPos < currAbsPos) {
+                        AudioClip.GetData(temp, readAbsPos % AudioClip.samples);
+
+                        Sample = temp;
+                        m_SampleCount++;
+                        if (OnSampleReady != null)
+                            OnSampleReady.Invoke(m_SampleCount, Sample);
+
+                        readAbsPos = nextReadAbsPos;
+                        isNewDataAvailable = true;
+                    }
+                    else
+                        isNewDataAvailable = false;
+                }
+                yield return null;
+            }
+        }
+        #endregion
+    }
+}
