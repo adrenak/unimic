319a5e562de506c967eafe2652efd71627e3e796
Vatsal Ambastha <ambastha.vatsal@gmail.com>
2024-12-09 09:07:56 +0530

    feat: v2.0.0
    BREAKING CHANGE:
    UniMic 2 breaks several v1 APIs.
    
    Mic.Device class now methods, properties and evets for recording. Mic class only offers access to an array of devices.
    
    Multiple recording devices can now be used in parallel.

---
+++ Editor/MicAudioSourceEditor.cs
@@ -0,0 +1,65 @@
+﻿#if UNITY_EDITOR
+
+using UnityEditor;
+
+using UnityEngine;
+
+namespace Adrenak.UniMic {
+    [CustomEditor(typeof(MicAudioSource))]
+    public class MicAudioSourceEditor : Editor {
+        private bool showInfo;
+
+        public override void OnInspectorGUI() {
+            base.OnInspectorGUI();
+
+            var me = (MicAudioSource)target;
+
+            if (me.myDevice == null && Mic.AvailableDevices.Count == 0) return;
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.PrefixLabel("Device");
+            var selectedName = Mic.AvailableDevices[0].Name;
+            if (me.myDevice != null) 
+                selectedName = me.myDevice.Name;
+
+            if (EditorGUILayout.DropdownButton(new GUIContent(selectedName), FocusType.Keyboard)) {
+                var menu = new GenericMenu();
+
+                foreach (var device in Mic.AvailableDevices)
+                    menu.AddItem(
+                        new GUIContent(device.Name),
+                        me.myDevice != null ? me.myDevice.Name == device.Name : false,
+                        OnDeviceSelected,
+                        device.Name
+                    );
+
+                menu.ShowAsContext();
+            }
+
+            EditorGUILayout.EndHorizontal();
+
+            if(me.myDevice != null) {
+                if (showInfo = EditorGUILayout.BeginFoldoutHeaderGroup(showInfo, "Debug Info")) {
+                    GUI.enabled = false;
+
+                    Microphone.GetDeviceCaps(me.myDevice.Name, out int min, out int max);
+                    EditorGUILayout.IntField("Max Frequency", max);
+                    EditorGUILayout.IntField("Min Frequency", min);
+                    EditorGUILayout.Toggle("Is Recording", me.myDevice.IsRecording);
+                    EditorGUILayout.IntField("Sampling Frequency", me.myDevice.SamplingFrequency);
+                    EditorGUILayout.IntField("Frame Duration (ms)", me.myDevice.FrameDurationMS);
+
+                    GUI.enabled = true;
+                }
+            }
+        }
+
+        // Handler for when a menu item is selected
+        private void OnDeviceSelected(object deviceName_) {
+            var deviceName = (string)deviceName_;
+            var me = (MicAudioSource)target;
+            me.SetDevice(deviceName);
+        }
+    }
+}
+#endif
--- Runtime/MicAudioSource.cs
+++ Runtime/MicAudioSource.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+
+using UnityEngine;
 
 namespace Adrenak.UniMic {
     /// <summary>
@@ -8,29 +10,71 @@ namespace Adrenak.UniMic {
     /// </summary>
     [RequireComponent(typeof(AudioSource))]
     public class MicAudioSource : MonoBehaviour {
-        public bool startRecordingAutomatically = true;
-        [Header("If startRecordingAutomatically is true:")]
-        public int recordingFrequency = 48000;
-        public int sampleDurationMS = 100;
+        public bool autoStart = true;
+        public int clipLengthMultiplier = 10;
+        public int frameDurationMS = 20;
+        public Mic.Device myDevice;
+
         AudioClip clip;
+        AudioSource audioSource;
+
+        int count = 0;
 
         void Start() {
-            var audioSource = gameObject.GetComponent<AudioSource>();
-            audioSource.loop = false;
+            Mic.Init();
 
-            var mic = Mic.Instance;
+            audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.loop = true;
 
-            if(startRecordingAutomatically)
-                mic.StartRecording(recordingFrequency, sampleDurationMS);
+            if (autoStart) {
+                if (Mic.AvailableDevices.Count == 0) {
+                    Debug.Log("There are no recording devices available");
+                    return;
+                }
+                Play(Mic.AvailableDevices[0]);
+            }
+        }
 
-            mic.OnTimestampedSampleReady += (timestamp, segment) => {
-                if (clip != null)
-                    Destroy(clip);
-                clip = AudioClip.Create("clip", segment.Length, mic.AudioClip.channels, mic.AudioClip.frequency, false);
-                clip.SetData(segment, 0);
-                audioSource.clip = clip;
-                audioSource.Play();
-            };
+        // We create an audio clip much longer than the frame length.
+        // This is because frames don't arrive at regular intervals and
+        // we don't want to overwrite the clip while we're still playing
+        // the last frame.
+        void OnStartRecording() {
+            if (clip != null)
+                Destroy(clip);
+            clip = AudioClip.Create(
+                "clip",
+                myDevice.FrameLength * clipLengthMultiplier,
+                myDevice.ChannelCount,
+                myDevice.SamplingFrequency,
+                false
+            );
+            audioSource.clip = clip;
+            audioSource.Play();
+        }
+
+        // Since the clip is longer than the length of samples we're expecting,
+        // we need to set the data at the right place by using an offset
+        void OnFrameCollected(int channels, float[] samples) {
+            clip.SetData(samples, (count % clipLengthMultiplier) * samples.Length);
+            count++;
+        }
+
+        public void SetDevice(string deviceName) {
+            if(myDevice != null) {
+                myDevice.StopRecording();
+                myDevice.OnStartRecording -= OnStartRecording;
+                myDevice.OnFrameCollected -= OnFrameCollected;
+            }
+            var newDevice = Mic.AvailableDevices.Where(x => x.Name == deviceName).First();
+            Play(newDevice);
+        }
+
+        void Play(Mic.Device device) {
+            myDevice = device;
+            myDevice.OnStartRecording += OnStartRecording;
+            myDevice.OnFrameCollected += OnFrameCollected;
+            myDevice.StartRecording(frameDurationMS);
         }
     }
 
